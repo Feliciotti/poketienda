15d8859f03439de1b99943b868f54cf0b1526115
Feliciotti <feliciotti@hotmail.com>
2023-11-23 22:41:40 -0300

    netlify not resolved files removed

--- src/context/CartContext.js
+++
@@ -1,6 +0,0 @@
-import {createContext, useContext} from 'react';
-
-const cartContext = createContext([])
-const useCartContext = () => useContext(cartContext)
-
-export {cartContext, useCartContext}
